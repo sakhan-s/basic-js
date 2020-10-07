72234a1600437a0a7a218518d8d48f2d108afc21
sakhan-s <siradzhov@ya.ru>
2020-10-07 10:36:20 +0300

    feat: added solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    chain: '',
+    chainLength: 0,
+    getLength() {
+        this.chainLength = this.chain.match(/( . )/);
+    },
+    addLink(value) {
+        if (this.chain === '') {
+            this.chain = '( ' + String(value) + ' )';
+            return this;
+        }
+        this.chain = this.chain + '~~( ' + String(value) + ' )';
+        return this;
+    },
+    removeLink(position) {
+        if (position >= 0 && position < this.chain.length && this.chain.length !== 0) {
+            if (this.chain.match(/( . )/) === 1) {
+                this.chain = '';
+                return this;
+            } else {
+                const a = this.chain.split('~~');
+                a.splice(position - 1, 1);
+                this.chain = a.join('~~');
+                return this;
+            }
+        } else {
+            this.chain = '';
+            throw new Error();
+        }
+    },
+    reverseChain() {
+        this.chain = this.chain.split('~~').reverse().join('~~');
+        return this;
+    },
+    finishChain() {
+        const res = this.chain.slice();
+        this.chain = '';
+        return res;
+    }
 };
 
 module.exports = chainMaker;
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (arr instanceof Array) {
+        let newArr = [];
+        let commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i]) {
+                case '--discard-next':
+                    if (i !== arr.length - 1) {
+                        newArr.push(arr[i]);
+                        i += 1;
+                    }
+                    break;
+                case '--discard-prev':
+                    if (i !== 0) {
+                        newArr.pop(arr[i - 1]);
+                        newArr.push(arr[i]);
+                    }
+                    break;
+                case '--double-next':
+                    if (i !== arr.length - 1) {
+                        newArr.push(arr[i], arr[i + 1]);
+                    }
+                    break;
+                case '--double-prev':
+                    if (i !== 0 && arr[i - 2] !== '--discard-next') {
+                        newArr.push(arr[i - 1], arr[i]);
+                    }
+                    break;
+                default:
+                    newArr.push(arr[i]);
+                    break;
+            }
+        }
+
+        let res = [];
+
+        newArr.forEach(element => {
+            if (commands.indexOf(element) === -1) {
+                res.push(element);
+            }
+        });
+        return res;
+    } else {
+        throw new Error();
+    }
 };
