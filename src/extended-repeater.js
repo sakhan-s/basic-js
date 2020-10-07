9efddc86ebaff86c04729af6ba07627e5bfbcd43
sakhan-s <siradzhov@ya.ru>
2020-10-07 10:18:34 +0300

    feat: added solution for fourth and fifth task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, option) {
+    let res = '';
+    str = String(str);
+    if (!option.hasOwnProperty('repeatTimes') || option.repeatTimes === undefined) {
+        option.repeatTimes = 1;
+    }
+    if (!option.hasOwnProperty('separator')) {
+        option.separator = '+';
+    }
+    if (!option.hasOwnProperty('addition')) {
+        option.addition = '';
+    } else {
+        option.addition = String(option.addition);
+    }
+    if (!option.hasOwnProperty('additionRepeatTimes') || option.additionRepeatTimes === undefined) {
+        option.additionRepeatTimes = 1;
+    }
+    if (!option.hasOwnProperty('additionSeparator')) {
+        option.additionSeparator = '|';
+    }
+
+    for (let i = 0; i < option.repeatTimes; i++) {
+        res += str;
+        for (let y = 0; y < option.additionRepeatTimes; y++) {
+            if (y === option.additionRepeatTimes - 1) {
+                res += option.addition;
+            } else {
+                res += option.addition + option.additionSeparator;
+            }
+        }
+
+        if (i !== option.repeatTimes - 1) {
+            res += option.separator;
+        }
+    }
+
+    return res;
 };
-  
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let t = 2 ** disksNumber - 1;
+    let s = Math.floor(3600 / turnsSpeed * t);
+    return { turns: t, seconds: s };
 };
